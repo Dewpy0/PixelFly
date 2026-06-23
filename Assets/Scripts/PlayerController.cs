8712d0381867270a001455cfb66e3e0560da27e6
Denis Mazurkevich <domovoj48@gmail.com>
2026-06-23 19:27:06 +0300

    add script PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+
+    private Rigidbody2D _rb;
+    private bool _pressJump;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Клик пойман!");
+            _pressJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_pressJump)
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+            _pressJump = false;
+        }
+    }
+}
